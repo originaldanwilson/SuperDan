a1498ff646c7db6060b164f35199ffe2e1d0e6c0
originaldanwilson <originaldanwilson@users.noreply.github.com>
2025-12-04 17:29:55 -0600

    Update Boring.py

--- Boring.py
+++ Boring.py
@@ -1,20 +1,30 @@
-from mj import jiggle_mouse
+#from mj import jiggle_mouse
 import time
 import pyautogui
+import random
 
 def alt_tab():
+    tabCount = random.randint(1, 5)
     pyautogui.keyDown('alt')
-    time.sleep(0.3)
-    pyautogui.press('tab')
-    time.sleep(0.5)
+    time.sleep(tabCount)
+    for i in range(tabCount):
+        pyautogui.keyDown('tab')
+        time.sleep(1)
     pyautogui.keyUp('alt')
-    time.sleep(1)
+
+def sleep_loop():
+    while True:
+        alt_tab()
+        x = random.randint(1, 60)
+        print(f"resting for {x} seconds ..")
+        time.sleep(x)
 
 def main():
-    while True:
-        #jiggle_mouse(interval=30)
-        alt_tab()
-        time.sleep(30)
+    sleep_loop()
+    #while True:
+    #    #jiggle_mouse(interval=30)
+    #    alt_tab()
+    #    time.sleep(30)
 
 if __name__ == "__main__":
     main()
